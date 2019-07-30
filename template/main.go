3e3b0c3edf24d9e12950d37a10b7ee7179e21ea5
Seth Pellegrino <spellegrino@newrelic.com>
2019-07-30 15:58:00 -0700

    template: Scaffold GuestBook API
    Generated by:
    ```
    kubebuilder create api --group webapp --kind GuestBook --version v1 --resource --controller
    make manifests
    ```

--- template/main.go
+++ template/main.go
@@ -19,6 +19,10 @@ import (
 	"flag"
 	"os"
 
+	webappv1 "guestbook-workshop/api/v1"
+
+	"guestbook-workshop/controllers"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -35,6 +39,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = webappv1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -58,6 +63,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.GuestBookReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("GuestBook"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "GuestBook")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
